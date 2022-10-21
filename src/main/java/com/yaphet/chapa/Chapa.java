0f6a77d42df893146a8790ee140f8a6f08e4451a
yaphet17 <yafetberhanu3@gmail.com>
2022-10-21 13:19:16 +0300

    Reformat code base

--- src/main/java/com/yaphet/chapa/Chapa.java
+++ src/main/java/com/yaphet/chapa/Chapa.java
@@ -12,63 +12,66 @@ import com.yaphet.chapa.client.ChapaClientImpl;
  */
 public class Chapa {
 
-    private final ChapaClient chapaClient;
     private static String responseBody;
-    private final String BASE_URL = "https://api.chapa.co/v1";
+    private final ChapaClient chapaClient;
+    private final String VERSION = "v1";
+    private final String BASE_URL = "https://api.chapa.co/" + VERSION;
     private final String SECRETE_KEY;
 
     /**
-     *
-     * @param secreteKey    A secrete key provided from Chapa.
+     * @param secreteKey A secrete key provided from Chapa.
      */
-    public Chapa(String secreteKey){
+    public Chapa(String secreteKey) {
         this.SECRETE_KEY = secreteKey;
         this.chapaClient = new ChapaClientImpl();
     }
 
     /**
-     *
-     * @param secreteKey    A secrete key provided from Chapa.
-     * @param chapaClient   Implementation of {@link com.yaphet.chapa.client.ChapaClient}
-     *                      which is used to make API calls to Chapa.
+     * @param secreteKey  A secrete key provided from Chapa.
+     * @param chapaClient Implementation of {@link com.yaphet.chapa.client.ChapaClient}
+     *                    which is used to make API calls to Chapa.
      */
-    public Chapa(ChapaClient chapaClient, String secreteKey){
+    public Chapa(ChapaClient chapaClient, String secreteKey) {
         this.chapaClient = chapaClient;
         this.SECRETE_KEY = secreteKey;
     }
 
 
     /**
-     *
-     * @param postData  Object of {@link com.yaphet.chapa.PostData} instantiated with
-     *                  post fields.
-     * @return          The current invoking object.
+     * @param postData Object of {@link com.yaphet.chapa.PostData} instantiated with
+     *                 post fields.
+     * @return         The current invoking object.
      */
-    public Chapa initialize(PostData postData) throws Throwable{
+    public Chapa initialize(PostData postData) throws Throwable {
         Util.validatePostData(postData);
 
         Map<String, Object> fields = new HashMap<>();
-        fields.put( "amount", postData.getAmount().toString());
+        fields.put("amount", postData.getAmount().toString());
         fields.put("currency", postData.getCurrency());
         fields.put("email", postData.getEmail());
         fields.put("first_name", postData.getFirst_name());
         fields.put("last_name", postData.getLast_name());
         fields.put("tx_ref", postData.getTx_ref());
 
-        String customization_title = postData.getCustomization_title();
-        String customization_description = postData.getCustomization_description();
-        String customization_logo = postData.getCustomization_logo();
+        String callbackUrl = postData.getCallback_url();
+        String customizationTitle = postData.getCustomization_title();
+        String customizationDescription = postData.getCustomization_description();
+        String customizationLogo = postData.getCustomization_logo();
 
-        if(customization_title != null && !customization_title.isEmpty()) {
-            fields.put("customization[title]", customization_title);
+        if (callbackUrl != null && !callbackUrl.isEmpty()) {
+            fields.put("callback_url", callbackUrl);
         }
 
-        if(customization_description != null && !customization_description.isEmpty()) {
-            fields.put("customization[description]", customization_description);
+        if (customizationTitle != null && !customizationTitle.isEmpty()) {
+            fields.put("customization[title]", customizationTitle);
         }
 
-        if(customization_logo != null && !customization_logo.isEmpty()) {
-            fields.put("customization[logo]", customization_logo);
+        if (customizationDescription != null && !customizationDescription.isEmpty()) {
+            fields.put("customization[description]", customizationDescription);
+        }
+
+        if (customizationLogo != null && !customizationLogo.isEmpty()) {
+            fields.put("customization[logo]", customizationLogo);
         }
 
         responseBody = chapaClient.post(BASE_URL + "/transaction/initialize", fields, SECRETE_KEY);
@@ -76,9 +79,8 @@ public class Chapa {
     }
 
     /**
-     *
-     * @param jsonData  JSON data which contains post fields.
-     * @return          The current invoking object.
+     * @param jsonData JSON data which contains post fields.
+     * @return The current invoking object.
      */
 
     public Chapa initialize(String jsonData) throws Throwable {
@@ -88,29 +90,26 @@ public class Chapa {
     }
 
     /**
-     *
-     * @param transactionRef   Unique transaction reference which was associated
-     *                         with tx_ref field in post data.
-     * @return                 The current invoking object.
+     * @param transactionRef Unique transaction reference which was associated
+     *                       with tx_ref field in post data.
+     * @return The current invoking object.
      */
-    public Chapa verify(String transactionRef) throws  Throwable{
+    public Chapa verify(String transactionRef) throws Throwable {
         responseBody = chapaClient.get(BASE_URL + "/transaction/verify/" + transactionRef, SECRETE_KEY);
         return this;
     }
 
     /**
-     *
-     * @return  String representation of the response JSON data.
+     * @return String representation of the response JSON data.
      */
-    public String asString(){
+    public String asString() {
         return responseBody;
     }
 
     /**
-     *
-     * @return  Map representation of the response JSON data.
+     * @return Map representation of the response JSON data.
      */
-    public Map<String, String> asMap(){
+    public Map<String, String> asMap() {
         return Util.jsonToMap(responseBody);
     }
 
