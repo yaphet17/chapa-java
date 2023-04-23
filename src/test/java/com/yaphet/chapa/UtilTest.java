001322966cc4f4310d9855d3a1ce879e065a21d6
yaphet17 <yafetberhanu3@gmail.com>
2023-04-23 16:24:58 +0300

    Ignore empty test class

--- src/test/java/com/yaphet/chapa/UtilTest.java
+++ src/test/java/com/yaphet/chapa/UtilTest.java
@@ -2,10 +2,12 @@ package com.yaphet.chapa;
 
 import com.yaphet.chapa.model.Customization;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Disabled
 class UtilTest {
     private final Map<String, String> customizations = new HashMap<>();
     private final Customization customization = new Customization();
